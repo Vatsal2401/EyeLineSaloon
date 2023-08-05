f44777c3691956757912f4574c4fae4449d2a89a
VATSAL PATEL <vatsalpatel9393@gmail.com>
2023-08-05 15:58:24 +0530

    Remaining Some changes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Blog from 'components/Blog';
+import Contact from 'components/Contact';
+import Footer from 'components/Footer';
+import Home from 'components/Home';
+import Pricing from 'components/Pricing';
+import ScrollToTop from 'components/ScrollToTop';
+import Services from 'components/Services';
+import Skills from 'components/Skills';
+import { motion } from 'framer-motion';
+import React from 'react';
+
+function App() {
+  return (
+    <motion.div initial="hidden" animate="show">
+      
+      <Home />
+      <Services />
+      {/* <Portfolio /> */}
+      {/* <Milestones /> */}
+      {/* <Blog /> */}
+      {/* <Video /> */}
+      <Pricing />
+      {/* <Testimonials /> */}
+      <Skills />
+      <Contact />
+      <Footer />
+      <ScrollToTop />
+    </motion.div>
+  )
+}
+
+export default App
